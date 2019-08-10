05a62cc41c56d73927a63d503afea3bb60bcf526
Kushal <kushshah777888@gmail.com>
2019-08-10 18:43:46 +0530

    Made migrations and updated admin pannel

--- RegisterApp/admin.py
+++ RegisterApp/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(MyUser)
+admin.site.register(GenderField)
+admin.site.register(PassoutBatch)
+admin.site.register(CourseName)
+admin.site.register(HigherEducation)
+admin.site.register(HostelName)
+admin.site.register(UserAddr)
+admin.site.register(SchoolAdd)
+admin.site.register(JobDescription)
+admin.site.register(HigherStudies)
