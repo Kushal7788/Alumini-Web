bff93181eec801d7a8e2851b261c0acdf57a91b0
Kushal <kushshah777888@gmail.com>
2019-08-10 20:53:07 +0530

    Added home page

--- Alumini/urls.py
+++ Alumini/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
-from django.conf.urls.static import static
-from django.conf import settings
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('RegisterApp.urls')),
+    url(r'^$', views.home, name='home'),
 ]
---
+++ Alumini/views.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.http.response import JsonResponse, HttpResponse
+from django.views.decorators.http import require_GET, require_POST
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from RegisterApp.templates import *
+
+
+def home(request):
+    return render(request, 'LoginTemplates/Login.html', None)
