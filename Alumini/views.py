b7a8eda505c27e56cbc0f924e13198b45a67c137
Kushal <kushshah777888@gmail.com>
2019-08-11 20:58:51 +0530

    Login functionality basic

--- Alumini/views.py
+++ Alumini/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'LoginTemplates/Login.html', None)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                # print("io")
+                login(request, user)
+                return render(request, 'LoginTemplates/Login.html', {})
+            else:
+                messages.error(request, 'Invalid Username or Password')
+                return render(request, 'LoginTemplates/Login.html', {})
+        else:
+            messages.error(request, 'Invalid Username or Password')
+            return render(request, 'LoginTemplates/Login.html', {})
+    else:
+        return render(request, 'LoginTemplates/Login.html', {})
