f3a71d84f7297b386d7c0bf4295c03b940c8c467
Kushal <kushshah777888@gmail.com>
2019-08-26 00:14:02 +0530

    Basic template changes

--- Alumini/static/registration/js/reset_confirm.js
+++ Alumini/static/registration/js/reset_confirm.js
@@ -1,5 +1,5 @@
-let new_password = document.getElementById('new_password1');
-let confirm_password =  document.getElementById('new_password2');
+let new_password = document.getElementById('id_new_password1');
+let confirm_password = document.getElementById('id_new_password2');
 
 new_password.classList.add("form-control");
 confirm_password.classList.add("form-control");
